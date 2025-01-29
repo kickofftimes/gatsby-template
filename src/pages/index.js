6f4f5be853dcdf97c97b9bf7d3a587d5d7c36bba
Si Jobling <si@jobling.com>
2025-01-29 20:38:42 +0000

    Reverse index date order and show year
    Signed-off-by: Si Jobling <si@jobling.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,7 +67,7 @@ const IndexPage = ({
 export default IndexPage
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: { order: ASC, fields: [frontmatter___date] }) {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
       edges {
         node {
           id
